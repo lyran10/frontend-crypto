4521cde2abbdd9d6ca73f32fa90397a2cc0faa28
lyran10 <liranramekar629@gmail.com>
2023-10-01 12:20:44 +0530

    update

--- src/component/coin/coinInfo.tsx
+++ src/component/coin/coinInfo.tsx
@@ -98,7 +98,7 @@ export const CoinInfo = ({ coinData }: Props) => {
           M
         </span>
         {userLogin ? 
-            <div className="flex flex-col mt-5 gap-2 w-[100%]">
+            <div className="flex flex-col mt-5 w-[100%]">
           <span className="text-center font-semibold text-[#f5f5f5]">
             Add {coinData.name} to your watch list
           </span>
