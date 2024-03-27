35bcbff5e46cf720651318d2f725a242dbbb7740
Liran Ramekar <liranramekar7@gmail.com>
2024-03-27 20:13:32 +0530

    added

--- src/component/redux/actions.tsx
+++ src/component/redux/actions.tsx
@@ -15,7 +15,6 @@ export const findUser = createAsyncThunk(
   async ({ id }: { id: string | undefined }, { rejectWithValue }) => {
     try {
       const { data } = await axios.post(`${process.env.REACT_APP_SERVER_URL}/api/expiry`, { id: id }, { withCredentials: true });
-      console.log(data)
       return data;
     } catch (error) {
       console.log(error)
@@ -46,7 +45,6 @@ export const login = createAsyncThunk(
     const { email, password } = user
     try {
       const { data } = await axios.post(`${process.env.REACT_APP_SERVER_URL}/api/login`, { email, password }, { withCredentials: true });
-      console.log(data)
       return data
     } catch (error) {
       if (axios.isAxiosError(error)) {
